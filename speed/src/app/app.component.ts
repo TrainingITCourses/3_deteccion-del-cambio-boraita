70fc91c3443ab39f2fc951dfdde3862574e17791
rmn <rmn@novatec-gmbh.de>
2018-11-10 14:56:57 +0100

    Created Critery filter

--- speed/src/app/app.component.ts
+++ speed/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'speed';
+  criteries;
+  criteryList;
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.criteries = [
+      { value: 'state', name: 'Estado' },
+      { value: 'agencies', name: 'Agencia' },
+      { value: 'type', name: 'Tipo' }
+    ];
+    this.criteryList = this.http
+      .get('../assets/data/launchstatus.json')
+      .pipe(map(data => data['types']));
+  }
+  criteryChanged(event) {
+    switch (event.target.value) {
+      case 'Agencia':
+        this.criteryList = this.http
+          .get('../assets/data/agencies.json')
+          .pipe(map(data => data['agencies']));
+        break;
+      case 'Estado':
+        this.criteryList = this.http
+          .get('../assets/data/launchstatus.json')
+          .pipe(map(data => data['types']));
+        break;
+      case 'Tipo':
+        this.criteryList = this.http
+          .get('../assets/data/missiontypes.json')
+          .pipe(map(data => data['types']));
+        break;
+    }
+  }
 }
---
+++ speed/src/app/filter-list/filter-list.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-list',
+  templateUrl: './filter-list.component.html',
+  styleUrls: ['./filter-list.component.css']
+})
+export class FilterListComponent {
+  @Input() filterList;
+}
